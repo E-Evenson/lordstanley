7cad2c8fd298a8cc98d7481992d560f9db9889d7
Eric Evenson <eric_evenson@outlook.com>
2026-04-09 18:02:09 -0600

    feat: add BigQuery load function

--- src/lord_stanley/pipeline/load.py
+++ src/lord_stanley/pipeline/load.py
@@ -11,9 +11,16 @@ Not responsible for:
     - What data to save
 """
 
+import logging
+from typing import Any
+
+from google.cloud import bigquery
 import pandas as pd
 
 from lord_stanley.config import PROCESSED_DIR
+
+
+logger = logging.getLogger(__name__)
 
 
 def save_schedule(df: pd.DataFrame, season: str) -> None:
@@ -27,3 +34,24 @@ def save_schedule(df: pd.DataFrame, season: str) -> None:
     PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
     df.to_csv(PROCESSED_DIR / f"{season}_schedule.csv", index=False)
     df.to_parquet(PROCESSED_DIR / f"{season}_schedule.parquet")
+
+
+def load_to_bigquery(raw_schedule: list[dict[str, Any]]) -> None:
+    """
+    Load raw schedule data to BigQuery
+
+    Args:
+        df: raw schedule data
+    """
+    logger.info("Writing raw schedule to BigQuery")
+
+    table_id = "lord-stanley.raw.schedule"
+
+    client = bigquery.Client()
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE, autodetect=True
+    )
+    job = client.load_table_from_json(raw_schedule, table_id, job_config=job_config)
+    job.result()
+
+    logger.info("Finished loading to BigQuery")
