7f9768dc7d09550c7065b96213d5faf86eb13657
Eric Evenson <eric_evenson@outlook.com>
2026-04-13 15:58:08 -0600

    fix: update stg_schedule model and add sources.yml

--- dbt/models/staging/stg_schedule.sql
+++ dbt/models/staging/stg_schedule.sql
@@ -1,24 +1,24 @@
 SELECT DISTINCT
-    currentSeason AS season,
-    id AS id,
-    gameType AS game_type,
-    gameDate AS game_date,
-    gameState AS game_state,
-    awayTeam.abbrev AS away_team_abbrev,
-    awayTeam.score AS away_team_score,
-    homeTeam.abbrev AS home_team_abbrev,
-    homeTeam.score AS home_team_score,
+    schedule.currentSeason AS season,
+    game.id AS id,
+    game.gameType AS game_type,
+    game.gameDate AS game_date,
+    game.gameState AS game_state,
+    game.awayTeam.abbrev AS away_team_abbrev,
+    game.awayTeam.score AS away_team_score,
+    game.homeTeam.abbrev AS home_team_abbrev,
+    game.homeTeam.score AS home_team_score,
     CASE
-        WHEN gameState NOT IN ('OFF', 'FINAL') THEN NULL
-        WHEN homeTeam.score > awayTeam.score THEN homeTeam.abbrev
-        ELSE awayTeam.abbrev
+        WHEN game.gameState NOT IN ('OFF', 'FINAL') THEN NULL
+        WHEN game.homeTeam.score > game.awayTeam.score THEN game.homeTeam.abbrev
+        ELSE game.awayTeam.abbrev
     END AS winner_abbrev,
     CASE
-        WHEN gameState NOT IN ('OFF', 'FINAL') THEN NULL
-        WHEN homeTeam.score > awayTeam.score THEN awayTeam.abbrev
-        ELSE homeTeam.abbrev
+        WHEN game.gameState NOT IN ('OFF', 'FINAL') THEN NULL
+        WHEN game.homeTeam.score > game.awayTeam.score THEN game.awayTeam.abbrev
+        ELSE game.homeTeam.abbrev
     END AS loser_abbrev
-FROM `lord-stanley.raw.schedule` AS schedule,
-UNNEST(games) as game
-WHERE gameType = 2
-ORDER BY id ASC
+FROM {{ source('raw', 'schedule') }} AS schedule
+CROSS JOIN UNNEST(schedule.games) as game -- Use CROSS JOIN for clarity
+WHERE game.gameType = 2
+ORDER BY game.id ASC
