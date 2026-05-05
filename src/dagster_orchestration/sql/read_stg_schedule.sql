b7896139d2d62ed25f3da6e5c4e2f814b47c71ad
Eric Evenson <eric_evenson@outlook.com>
2026-05-04 18:59:34 -0600

    fix: enforce game order from BigQuery for cup possession logic

--- src/dagster_orchestration/sql/read_stg_schedule.sql
+++ src/dagster_orchestration/sql/read_stg_schedule.sql
@@ -1,2 +1,3 @@
 SELECT season, id, game_type, game_date, start_time, game_state, away_team_abbrev, away_team_score, home_team_abbrev, home_team_score, winner_abbrev, loser_abbrev
 FROM `lord-stanley.staging.stg_schedule`
+ORDER BY id ASC
